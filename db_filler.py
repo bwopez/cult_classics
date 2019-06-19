48a090c5cd5b421f2c7928fa6aa227edf045d89b
matthew <matthewbropez@gmail.com>
2019-06-19 13:55:07 -0400

    Getting the year in movie_titles is pretty good but I get rate limited when filling the db

--- db_filler.py
+++ db_filler.py
@@ -3,14 +3,18 @@ Script that will fill the database with cult classic films from the wikipedia pa
 """
 from string import ascii_uppercase
 import movie_titles
+import imdb_info
 
 
 base_link = 'https://en.wikipedia.org/wiki/List_of_cult_films:_'
 titles = []
 for char in ascii_uppercase:
     webpage = base_link + char
-    titles.append(movie_titles.get_titles(webpage)['nice'])
+    titles.append(movie_titles.get_titles(webpage))
 
-for thing in titles:
-    for movie in thing:
-        print(movie)
+# for thing in titles:
+#     for movie in thing:
+#         print(movie)
+        # year, title = movie.split('||')
+        # info = imdb_info.get_movie_info(title, int(year))
+        # print(title, info['director'])
--- movie_titles.py
+++ movie_titles.py
@@ -3,6 +3,25 @@ Script to get all movies from a web page
 """
 from bs4 import BeautifulSoup as bs
 import requests
+import wikipedia
+import re
+
+
+def get_year(title, lang='en'):
+    """
+
+    :param title:
+    :param lang:
+    :return:
+    """
+    wikipedia.set_lang(lang)
+    summary = wikipedia.WikipediaPage(title=title).summary
+    match = re.search('\d{4}', summary)
+    if match:
+        year = match.group(0)
+    else:
+        year = "unknown"
+    return year
 
 
 def get_titles(webpage):
@@ -10,51 +29,41 @@ def get_titles(webpage):
     A function to get the titles from a wikipedia webpage that was passed in.
     This was an ad-hoc script to get all the movies from the Cult Classic Films table,
     there are no guarantees that this will work on any other wikipedia page let alone web page.
-    The all_titles object has two attributes that need to be explained
-
-    ['nice']    - titles that worked well with the script
-    ['naughty'] - titles that didn't work well with the script
-                    ie. the years are formatted different or it is a series
-                    not to be misinterpreted as titles of 18+ movies
 
     :param webpage: A link to the wikipedia webpage that has movies to scrape
 
-    :return: Object with ['nice'] titles that worked well with script and ['naughty'] titles that didn't
+    :return: all titles from the wikipedia table
     """
     page = requests.get(webpage)
     soup = bs(page.text, 'html.parser')
 
     table = soup.find('table', {'class': 'wikitable'})
-    all_titles = dict()
     titles = []
-    troublesome_titles = []
     for italic in table.find_all('i'):
         if italic.find('a'):
             title = italic.find('a')['title']
-            if 'l:' in title:
-                title = title.split('l:')[1].strip()
-            year = italic.findNext('td').get_text().strip()
+            language = 'en'
+            match = re.search('[a-z][a-z]:', title)
+            if match and match.group(0) == title[:len(match.group(0))]:
+                language, title = title.split(match.group(0)[-1])
+            year = get_year(title, language)
+
             title = year + '||' + title
             if '(' in title:
                 title = title.split('(')[0]
 
-            if len(year) == 4 and '-' not in year:
-                titles.append(title)
-            else:
-                troublesome_titles.append(title)
+            titles.append(title)
+            print(title)
 
-    all_titles['nice'] = titles
-    all_titles['trouble'] = troublesome_titles
-
-    return all_titles
+    return titles
 
 
 if __name__ == '__main__':
-    print("Doing a trial run. Grabbing all titles from 'cult classics: A'")
-    my_titles = get_titles('https://en.wikipedia.org/wiki/List_of_cult_films:_W')
-    print("Titles have been loaded. Printing titles")
-    print("========================================")
-    print("========================================")
-    print("========================================")
-    for year_title in my_titles:
-        print(year_title)
+    print("Doing a trial run. Grabbing all titles from 'cult classics: C'")
+    my_titles = get_titles('https://en.wikipedia.org/wiki/List_of_cult_films:_C')
+    # print("Titles have been loaded. Printing titles")
+    # print("========================================")
+    # print("========================================")
+    # print("========================================")
+    # for year_title in my_titles:
+    #     print(year_title)
