b95283a5bcd2a4e8f90dae22cd4d946ee9697c04
matthew <matthewbropez@gmail.com>
2019-06-25 16:35:52 -0400

    Changed a couple of the settings

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('CC_NAME', ''),
+        'USER': os.environ.get('CC_USER', ''),
+        'PASSWORD': os.environ.get('CC_PASS', ''),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
